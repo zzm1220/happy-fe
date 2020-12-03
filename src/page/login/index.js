f89139c2e0ea3826f06e1d4456924273a2dd8665
zhaozhimin <zhaozhimin@chinaiptv.cn>
2020-12-03 17:40:49 +0800

    登陆页面

--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,4 +1,25 @@
-const common = require("../common");
-console.log(common.a);
+/*
+ * @Author: zhimin
+ * @Date: 2020-12-01 16:26:35
+ * @LastEditors: zhimin
+ * @LastEditTime: 2020-12-03 17:40:19
+ * @FilePath: \happy-fe\src\page\login\index.js
+ */
+import "./index.css";
+import navSimple from "page/common/nav-simple";
+import util from "util";
+import common from "page/common";
 
-console.log("login");
+
+const login = {
+    init() {
+        this.bindEvent();
+    },
+    bindEvent() {
+
+    }
+};
+
+$(function () {
+    login.init();
+});
