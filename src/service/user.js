03ddc6dc334e91bb5a1a18bf5c20d73ce238db2c
zhaozhimin <zhaozhimin@chinaiptv.cn>
2020-12-04 10:51:10 +0800

    login completed

--- src/service/user.js
+++ src/service/user.js
@@ -2,12 +2,21 @@
  * @Author: zhimin
  * @Date: 2020-12-02 13:57:33
  * @LastEditors: zhimin
- * @LastEditTime: 2020-12-02 14:08:41
+ * @LastEditTime: 2020-12-04 10:35:58
  * @FilePath: \happy-fe\src\service\user.js
  */
 import util from "util";
 
 const _user = {
+    login(userInfo, resolve, reject) {
+        util.request({
+            url: util.getServerUrl("/api/user/login.do"),
+            method: 'POST',
+            data: userInfo,
+            success: resolve,
+            error: reject
+        })
+    },
     // 退出登录
     logout(resolve, reject) {
         util.request({
